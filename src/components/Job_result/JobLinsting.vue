4fc50cf0c486cdc622a1c291e2934e84071e2206
Miguel Abache <miiguel.abache@gmail.com>
2023-02-11 14:29:39 -0400

    fake backend and listing jobs

--- src/components/Job_result/JobLinsting.vue
+++ src/components/Job_result/JobLinsting.vue
@@ -1,19 +1,27 @@
 <template>
   <li class="mb-7">
     <router-Link
-      to="/jobs/results/1"
+      :to="jobPageLink"
       class="block mx-auto bg-white border border-solid border-brand-gray-2 rounded hover:shadow-gray"
     >
       <div class="pt-5 pb-2 mx-8 border-b border-solid border-brand-gray-2">
         <h2 class="mb-2 text-2xl">
-          Technical Program Manager, Perception, Augmented Reality
+          {{ props.job.title }}
         </h2>
         <div class="flex flex-row align-middle">
           <div class="mr-5">
-            <span>Bobo </span>
+            <span>{{ props.job.organization }}</span>
           </div>
           <div>
-            <span>San Francisco, CA,</span>
+            <ul>
+              <li
+                v-for="location in props.job.locations"
+                :key="location"
+                class="inline-block mr-5"
+              >
+                {{ location }}
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -22,18 +30,18 @@
           <h3 class="mt-1 mb-2">Qualifications:</h3>
           <div>
             <ul class="pl-8 list-disc">
-              <li>Bachelor's degree or equivalent practice experiences.</li>
-              <li>5 years of experience in program management.</li>
-              <li>
-                Exoerience analyzing data through querying database (e.g.SQL),
-                using spredsheeat software, and creating statistical model.
+              <li
+                v-for="qualification in props.job.minimumQualifications"
+                :key="qualification"
+              >
+                {{ qualification }}
               </li>
             </ul>
           </div>
         </div>
 
         <div class="mt-2 text-center">
-          <router-Link to="/jobs/results/1" class="text-brand-blue-1"
+          <router-Link :to="jobPageLink" class="text-brand-blue-1"
             >Expand</router-Link
           >
         </div>
@@ -42,6 +50,21 @@
   </li>
 </template>
 
-<script setup></script>
+<script setup>
+import { defineProps, computed } from "vue";
+const props = defineProps({
+  job: {
+    type: Object,
+    require: true,
+    default() {
+      return [];
+    },
+  },
+});
+
+const jobPageLink = computed(() => {
+  return `/jobs/results/${props.job.id}`;
+});
+</script>
 
 <style lang="scss" scoped></style>
--- src/components/Job_result/JobListings.vue
+++ src/components/Job_result/JobListings.vue
@@ -1,15 +1,26 @@
 <template>
   <main class="flex-auto p-8 bg-brand-gray-2">
     <ol>
-      <JobListing />
-      <JobListing />
-      <JobListing />
+      <JobListing v-for="job in jobs.jobs" :key="job.id" :job="job" />
     </ol>
   </main>
 </template>
 
 <script setup>
+import axios from "axios";
 import JobListing from "@/components/Job_result/JobLinsting.vue";
+import { reactive, onMounted } from "vue";
+
+let jobs = reactive({ jobs: [] });
+onMounted(() => {
+  getJobs();
+});
+
+const getJobs = async () => {
+  await axios.get("http://localhost:3000/jobs").then((response) => {
+    jobs.jobs = response.data;
+  });
+};
 </script>
 
 <style scoped></style>
